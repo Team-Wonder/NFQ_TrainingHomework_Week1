685eb14b523086d93ca08ebddaa7439d7724f026
mrminhb <novafalcongamers@gmail.com>
2023-05-22 16:36:58 +0700

    Add some UI functionalities

--- index.js
+++ index.js
@@ -1,28 +1,48 @@
 const dogCtn = document.querySelector("#dog")
 const catCtn = document.querySelector("#cat")
 const fishCtn = document.querySelector("#fish")
+const nameCtn = document.querySelector("#name-ctn")
 
 const petSelectionCtn = document.querySelector("#pet-selection-ctn")
 const selectedPetCtn = document.querySelector("#selected-pet-ctn")
 
+const backBtn = document.querySelector("#back-btn")
+const nameEditBtn = document.querySelector("#name-edit-btn")
+
 let pet = {}
 
-function toggleSelectionDisplay() {
+function toggleSelectionDisplayOff() {
     petSelectionCtn.style.display = "none"
     selectedPetCtn.style.display = "flex"
 }
 
 dogCtn.addEventListener("click", () => {
     console.log("dog")
-    toggleSelectionDisplay()
+    toggleSelectionDisplayOff()
 })
 
 catCtn.addEventListener("click", () => {
     console.log("cat")
-    toggleSelectionDisplay()
+    toggleSelectionDisplayOff()
 })
 
 fishCtn.addEventListener("click", () => {
     console.log("fish")
-    toggleSelectionDisplay()
+    toggleSelectionDisplayOff()
+})
+
+nameEditBtn.addEventListener("click", () => {
+    nameCtn.innerHTML = `
+        <label>Enter your pet name: </label>
+        <input type="text">
+        <div>
+            <button>Save</button>
+            <button>Cancel</button>
+        </div>
+    `
+})
+
+backBtn.addEventListener("click", () => {
+    petSelectionCtn.style.display = "flex"
+    selectedPetCtn.style.display = "none"
 })
